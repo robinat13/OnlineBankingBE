b475a7f14d1a64469cb1126e69e63b15b347182d
robinat13 <robinthehood13@gmail.com>
2019-11-19 14:30:14 +0530

    Update OnlinebankApplicationTests.java

--- src/test/java/com/cg/onlinebank/OnlinebankApplicationTests.java
+++ src/test/java/com/cg/onlinebank/OnlinebankApplicationTests.java
@@ -1,16 +1,16 @@
-package com.cg.onlinebank;
+// package com.cg.onlinebank;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class OnlinebankApplicationTests {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class OnlinebankApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+// 	@Test
+// 	public void contextLoads() {
+// 	}
 
-}
+// }
